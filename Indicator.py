9796eb19a2ad655425903a148328f47aace236e7
Oasisks <oasisks2@gmail.com>
2021-06-05 20:55:38 -0400

    Initializing Files

---
+++ Indicator.py
@@ -0,0 +1,7 @@
+
+
+class Indicator:
+    def __init__(self):
+        pass
+
+
---
+++ Trend.py
@@ -0,0 +1,8 @@
+from Indicator import Indicator
+
+
+class TrendLine(Indicator):
+    def __init__(self):
+        super(TrendLine, self).__init__()
+        pass
+
