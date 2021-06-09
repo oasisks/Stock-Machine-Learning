62d089542e746cb91a2ba26bc9a30a39b4b062a5
Oasisks <oasisks2@gmail.com>
2021-06-08 20:28:39 -0400

    Created and formatted some data from the API

--- StockData.py
+++ StockData.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-class Stock:
+class StockDatumEntry:
     def __init__(self, rawData: dict):
         self._rawData = rawData
 
@@ -30,6 +30,47 @@ class Stock:
     def volume(self):
         return float(self._rawData["volume"])
 
+    @property
+    def indicators(self):
+        return
+
+
+class Indicators:
+    def __init__(self, rawData):
+        self._rawData = rawData
+
+    @property
+    def adx(self):
+        return float(self._rawData["adx"])
+
+    @property
+    def bbands(self):
+        return {"upper_band": float(self._rawData["upper_band"]),
+                "middle_band": float(self._rawData["middle_band"]),
+                "lower_band": float(self._rawData["lower_band"])}
+
+    @property
+    def ema(self):
+        return float(self._rawData["ema"])
+
+    @property
+    def macd(self):
+        return {"macd": float(self._rawData["macd"]),
+                "macd_signal": float(self._rawData["macd_signal"]),
+                "macd_hist": float(self._rawData["macd_hist"])}
+
+    @property
+    def percentB(self):
+        return float(self._rawData["percent_b"])
+
+    @property
+    def rsi(self):
+        return float(self._rawData["rsi"])
+
+    @property
+    def stoch(self):
+        pass
+
 
 def processRawDateTime(rawDateTime):
     # year-month-date hour-minute-second
@@ -52,4 +93,4 @@ if __name__ == '__main__':
     data = {'datetime': '2021-06-08 12:05:00', 'open': '1.13500',
             'high': '1.14000', 'low': '1.13000', 'close': '1.14000',
             'volume': '472793'}
-    stock = Stock(data)
+    stock = StockDatumEntry(data)
--- Temp.py
+++ Temp.py
@@ -2,7 +2,7 @@ from twelvedata import TDClient
 from dotenv import load_dotenv
 from json import dump, load
 from datetime import datetime
-from StockData import Stock, processRawDateTime
+from StockData import StockDatumEntry, processRawDateTime
 import os
 
 
@@ -32,27 +32,27 @@ def priceDifference(stockData: dict, startDate: str, endDate: str) -> float:
 # Twelve Data GitHub: https://github.com/twelvedata/twelvedata-python
 
 
-td = TDClient(apikey=APIKEY)
-ts = td.time_series(
-    symbol="SNDL",
-    interval="1day",
-    timezone="America/New_York",
-    start_date="2020-05-01",
-    end_date="2021-05-02",
-    outputsize=5000
-)
+# td = TDClient(apikey=APIKEY)
+# ts = td.time_series(
+#     symbol="SNDL",
+#     interval="1day",
+#     timezone="America/New_York",
+#     start_date="2020-05-01",
+#     end_date="2021-05-02",
+#     outputsize=5000
+# )
+#
+# saveJsonFile("data.json", ts.with_adx().with_bbands().with_ema().with_macd().with_percent_b().with_rsi().with_stoch().as_json())
 
-saveJsonFile("tempData.json", ts.as_json())
-
-file = open("tempData.json", "r", encoding="utf-8")
+file = open("data.json", "r", encoding="utf-8")
 
 data = load(file)
 
 stockData = {}
 for index, datum in enumerate(data):
     print(datum)
-    stock = Stock(datum)
+    stock = StockDatumEntry(datum)
     stockData[stock.datetime] = stock
 
-print(stockData)
-print(priceDifference(stockData, "2020-05-01", "2021-04-30"))
+# print(stockData)
+# print(priceDifference(stockData, "2020-05-01", "2021-04-30"))
