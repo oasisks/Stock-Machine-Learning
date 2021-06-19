3d7eabf8c6fc9c2770182aae92e8a2ad5cb2d88a
Oasisks <oasisks2@gmail.com>
2021-06-18 20:15:28 -0400

    Implemented EMA

--- Indicators.py
+++ Indicators.py
@@ -32,14 +32,14 @@ class Indicators:
 
             sumOfClose = sum(queue)
             sma = sumOfClose / len(queue)
-            smas[date] = sma
+            standardDevation = np.std(queue, dtype=np.float32)
+            smas[date] = sma, standardDevation
 
         for key, value in smas.items():
             print(key, value)
         return smas
 
     def ADX(self, periods=14):
-
         pass
 
     def BBands(self, periods=20, std=2):
@@ -49,29 +49,52 @@ class Indicators:
         :param std: the number of standard deviation
         :return: dict
         """
-        queue = []
+        smas = self.SMA(periods=periods)
         bbands = {}
-        for index in range(len(self._df.index)):
-            if len(queue) > periods:
-                queue.pop(0)  # pop the first one out
 
-            date = self._df.Date[index]
-            close = self._df.Close[index]
+        for date, smaStd in smas.items():
+            (sma, standardDeviation) = smaStd
+            upperBand = sma + std * standardDeviation
+            lowerBand = sma - std * standardDeviation
 
-            queue.append(float(close))
-
-            standardDeviation = np.std(queue, dtype=np.float32)
-
-            middleBand = sum(queue) / len(queue)
-            upperBand = middleBand + standardDeviation * std
-            lowerBand = middleBand - standardDeviation * std
-
-            bbands[date] = {"upperBand": upperBand, "middleBand": middleBand, "lowerBand": lowerBand}
+            bbands[date] = {"middleBand": sma, "upperBand": upperBand, "lowerBand": lowerBand}
 
         return bbands
 
-    def EMA(self):
-        pass
+    def EMA(self, periods=10):
+        """
+
+        :param periods: the number of periods
+        :return: dict
+        """
+        # Calculate SMA
+        smas = self.SMA(periods=periods)
+
+        # Calculate multiplier
+        multiplier = 2 / (periods + 1)
+
+        # Calculate EMA
+        emas = {}
+        dates = []
+        for index in range(len(self._df.index)):
+            date = self._df.Date[index]
+            close = self._df.Close[index]
+            # we can only calculate ema after period
+            if index < periods - 1:
+                emas[date] = None
+                dates.append(date)
+                continue
+            elif index == periods - 1:
+                # the ema for this day will just be the simple moving average of that day
+                emas[date] = smas[date]
+                dates.append(date)
+                continue
+
+            ema = close * multiplier + emas[dates[index - 1]] * (1 - multiplier)
+
+            emas[date] = ema
+
+        return emas
 
     def MACD(self):
         pass
